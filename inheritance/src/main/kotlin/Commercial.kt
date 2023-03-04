07d324b4a802087421b210338d3c358f971ddb07
Sam Davenport <sdavenport@students.mchenry.edu>
2023-03-03 21:52:10 -0600

    initial commit

--- inheritance/src/main/kotlin/Commercial.kt
+++ inheritance/src/main/kotlin/Commercial.kt
@@ -1,2 +1,56 @@
-class Commercial {
+class Commercial(propName:String, rate:Double, multiProp:String,
+                custName:String,
+                custPhone:String,
+                custAddress:String,
+                sqrFoot:Double):
+    Customer(custName, custPhone, custAddress, sqrFoot) {
+    var propName:String = ""
+        get() = field
+        set(value) {
+            field = value
+        }
+    var rate:Double = 5.0
+        get() = field
+        set(value) {
+            field = value
+        }
+    var multiProp:String = ""
+        get() = field
+        set(value) {
+            field = value
+        }
+
+        init {
+            this.propName = propName
+            this.rate = rate
+            this.multiProp = multiProp
+            commercial()
+        }
+    public fun commercial(){
+        var totalCost = 0.0
+        propName = println("What is the Property Name?: ").toString()
+        propName = readLine()!!.toString()
+        multiProp = println("Do you have multiple properties? (yes/no): ").toString()
+        multiProp = readLine()!!.toString()
+        custName = println("Enter your name: ").toString()
+        custName = readLine()!!.toString()
+        custAddress = println("Enter your address: ").toString()
+        custAddress = readLine()!!.toString()
+        custPhone = println("Enter your phone number: ").toString()
+        custPhone = readLine()!!.toString()
+        sqrFoot = println("Enter the square footage: ")
+        sqrFoot = readln().toDouble()
+        rate = 5.0
+        if (multiProp == "no"){
+            rate = 5.0
+            var totalCost = rate * sqrFoot
+        }else{
+            rate = 5.0 - (5.0 * 0.1)
+            var totalCost = rate * sqrFoot
+        }
+
+        println("'$custName, $custAddress, $custPhone, $propName, $sqrFoot square ft, $$rate/1000 sqrfeet, Multiple Prop: $multiProp, Weekly Charge: $totalCost'")
+
+    }
+
 }
--- inheritance/src/main/kotlin/Residential.kt
+++ inheritance/src/main/kotlin/Residential.kt
@@ -1,2 +1,44 @@
-class Residential {
+class Residential(rateR:Double, senior:String, custName:String,
+                  custPhone:String,
+                  custAddress:String,
+                  sqrFoot:Double):
+        Customer(custName, custPhone, custAddress, sqrFoot) {
+    var rateR:Double = 6.0
+        get() = field
+        set(value) {
+            field = value
+        }
+    var senior:String = ""
+        get() = field
+        set(value) {
+            field = value
+        }
+
+    init {
+        this.rateR = rateR
+        residential()
+    }
+    fun residential(){
+        var totalCost = 0.0
+        custName = println("Enter your name: ").toString()
+        custName = readLine()!!.toString()
+        custAddress = println("Enter your address: ").toString()
+        custAddress = readLine()!!.toString()
+        custPhone = println("Enter your phone number: ").toString()
+        custPhone = readLine()!!.toString()
+        senior = println("Are you a 55 years or older? (yes/no): ").toString()
+        senior = readLine()!!.toString()
+        sqrFoot = println("Enter the square footage: ")
+        sqrFoot = readLine()!!.toDouble()
+        rateR = 6.0
+        if (senior == "no"){
+            rateR = 6.0
+            var totalCost = rateR * sqrFoot
+        }else{
+            rateR = 6.0 - (6.0 * 0.15)
+            var totalCost = rateR * sqrFoot
+        }
+
+        println("'$custName, $custAddress, $custPhone, 55 or older: $senior, $sqrFoot square ft, $$rateR/1000 sqrfeet, Weekly Charge: $totalCost'")
+    }
 }
